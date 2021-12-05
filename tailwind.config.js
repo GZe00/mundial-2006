dd7f9d03c5e7547bf3a9bff1607160c9cda426bd
Moises Fuentes <dev.gze.moises@gmail.com>
2021-12-05 12:08:18 -0600

    Home

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "mundial-black": "#000000",
+        "mundial-green": "#3E5413",
+        "mundial-yellow": "#DEBD3C",
+        "mundial-orange": "#F08B35",
+        "mundial-red": "#EB3A34",
+      }
+    },
   },
   variants: {
     extend: {},
