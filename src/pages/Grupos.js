3affd775c83c0873f65a1cac4d2d35f94d49c43c
Moises Fuentes <dev.gze.moises@gmail.com>
2021-12-07 15:03:30 -0600

    Navegacion incluida

--- src/pages/Grupos.js
+++ src/pages/Grupos.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navegacion from '../components/Navegacion'
 
 const Grupos = () => {
     return (
         <div>
+            <Navegacion />
             Grupos
         </div>
     )
