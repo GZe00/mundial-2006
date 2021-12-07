011a906a3a65fa000d2b618282f092e234801b04
Moises Fuentes <dev.gze.moises@gmail.com>
2021-12-07 14:47:07 -0600

    Correcion de errores

--- src/pages/Calendario.js
+++ src/pages/Calendario.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navegacion from '../components/Navegacion'
+import CardGrupos from '../components/CardGrupos'
 
 const Calendario = () => {
     return (
-        <div>
-            Calendario
+        <div className='w-full h-screen'>
+            <Navegacion />
+            <div className='flex flex-wrap justify-evenly'>
+            <CardGrupos grupo="A" equipos={['Alemania', 'Costa Rica', 'Polonia', 'Ecuador']}/>
+            <CardGrupos grupo="B" equipos={['Inglaterra', 'Paraguay', 'Trinidad y Tobago', 'Suecia']}/>
+            <CardGrupos grupo="C" equipos={['Argentina', 'Costa de Marfil', 'Serbia y Montenegro', 'Holanda']}/>
+            <CardGrupos grupo="D" equipos={['México', 'Irán', 'Angola', 'Portugal']}/>
+            <CardGrupos grupo="E" equipos={['Italia', 'Ghana', 'Estados Unidos', 'República Checa']}/>
+            <CardGrupos grupo="F" equipos={['Brasil', 'Croacia', 'Australia', 'Japón']}/>
+            <CardGrupos grupo="G" equipos={['Francia', 'Suiza', 'República de Corea', 'Togo']}/>
+            <CardGrupos grupo="H" equipos={['España', 'Ucrania', 'Túnez', 'Arabia Saudita']}/>
+            </div>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import '../assets/effect_opacity_gradient.css'
 import EstadioBerlin from '../assets/estadios/berlin.jpg'
+import Navegacion from '../components/Navegacion';
 
 const Home = () => {
 
@@ -15,25 +16,25 @@ const Home = () => {
             <div className='effect z-10 top-0 absolute w-full h-full' />
             <img className='left-0 z-0 top-0 absolute object-cover w-full h-full' style={{ opacity: '1' }} src={EstadioBerlin} />
 
-            <nav className='absolute text-white my-8 z-50 right-0'>
+            <nav className='absolute text-white py-8 z-50 right-0'>
                 <ul className='flex justify-end'>
-                    <Link className='text-2xl mx-4 cursor-pointer font-semibold hover:underline' to='/calendario'>Calendario</Link>
-                    <Link className='text-2xl mx-4 cursor-pointer font-semibold hover:underline' to='/grupos'>Grupos</Link>
-                    <Link className='text-2xl mx-4 cursor-pointer font-semibold hover:underline' to='/paises'>Paises</Link>
-                    <Link className='text-2xl mx-4 cursor-pointer font-semibold hover:underline' to='/historia'>Historia</Link>
-                    <Link className='text-2xl mx-4 cursor-pointer font-semibold hover:underline' to='/leyendas_del_futbol'>Leyendas del Fútbol</Link>
+                    <Link className={`text-white text-2xl mx-4 cursor-pointer font-semibold hover:underline`} to='/calendario'>Calendario</Link>
+                    <Link className={`text-white text-2xl mx-4 cursor-pointer font-semibold hover:underline`} to='/grupos'>Grupos</Link>
+                    <Link className={`text-white text-2xl mx-4 cursor-pointer font-semibold hover:underline`} to='/paises'>Paises</Link>
+                    <Link className={`text-white text-2xl mx-4 cursor-pointer font-semibold hover:underline`} to='/historia'>Historia</Link>
+                    <Link className={`text-white text-2xl mx-4 cursor-pointer font-semibold hover:underline`} to='/leyendas_del_futbol'>Leyendas del Fútbol</Link>
                 </ul>
             </nav>
             <div className='absolute w-full h-screen flex items-end pb-32 text-4xl justify-center z-30'>
-                <Link to='/alemania_2006' className='text-white rounded p-3 cursor-pointer' style={{border: '5px solid #DEBD3C'}}>
+                <Link to='/alemania_2006' className='text-white rounded p-3 cursor-pointer' style={{ border: '5px solid #DEBD3C' }}>
                     Comenzar
                 </Link>
             </div>
             <div className='relative z-20 h-full flex flex-col justify-center ml-6'>
-                <div className='max-w-max h-auto p-6 rounded' style={{backgroundColor: 'rgba(255,255,255,0.45)'}}>
-                    <p className='text-8xl' style={{color: '#000000', textShadow: '1px 1px 2px black'}}>Alemania 2006</p>
-                    <p className='my-4 text-5xl' style={{color: 'red', textShadow: '1px 1px 2px black'}} >Un vistazo al pasado</p>
-                    <p className='text-xl font-semibold' style={{color: 'yellow', textShadow: '1px 1px 2px black'}}>Descubre la cultura e historia del mundo de hace <span className='underline'>{yearCurrent - 2006} años</span></p>
+                <div className='max-w-max h-auto p-6 rounded' style={{ backgroundColor: 'rgba(255,255,255,0.45)' }}>
+                    <p className='text-8xl' style={{ color: '#000000', textShadow: '1px 1px 2px black' }}>Alemania 2006</p>
+                    <p className='my-4 text-5xl' style={{ color: 'red', textShadow: '1px 1px 2px black' }} >Un vistazo al pasado</p>
+                    <p className='text-xl font-semibold' style={{ color: 'yellow', textShadow: '1px 1px 2px black' }}>Descubre la cultura e historia del mundo de hace <span className='underline'>{yearCurrent - 2006} años</span></p>
                 </div>
             </div>
 
