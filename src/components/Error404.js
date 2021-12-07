3a0f87554f2106dcc1fabb68ea58e1ea95092bba
Moises Fuentes <dev.gze.moises@gmail.com>
2021-12-07 15:03:53 -0600

    Muestra la pagina no disponible

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import BG from '../assets/bg-1.png';
 import '../assets/Fonts.css'
-import Navegacion from './Navegacion';
+import { useHistory } from "react-router-dom";
 
 const Error404 = () => {
+    let history = useHistory()
+    let pathName = ((((history.location.pathname).split("/")).join("")).split("_")).join(" ");
+
+    const [pathCurrent, setPathCurrent] = React.useState('');
+    
+    React.useEffect(() => {
+        setPathCurrent(pathName[0].toUpperCase() + pathName.replace(pathName[0], ""))
+    }, [pathName])
+
     return (
         <div className='w-full h-screen relative' >
             <nav className='absolute text-white py-8 z-50 right-0'>
@@ -19,7 +28,7 @@ const Error404 = () => {
             <img src={BG} className='absolute w-full h-full object-cover' style={{ zIndex: '-1' }} />
             <div className='flex flex-col h-full items-center justify-center pb-4'>
                 <p className='text-9xl' style={{fontFamily: 'Meow Script'}}>404</p>
-                <p className='text-6xl my-3'>Error 404 - Página no encontrada</p>
+                <p className='text-6xl my-3 text-center'>Error 404 - Página "<span>{pathCurrent ?? '--'}</span>" no encontrada</p>
                 <p className='text-2xl mt-4'>Estoy aún construyendo el sitio y estará lista en 3, 5... quizás 7</p>
                 <Link className='mt-6' to='/' style={{backgroundColor: 'rgba(251,255,0, 0.15)'}}><p className='text-xl rounded-lg px-12 p-3 cursor-pointer z-20' style={{ border: '3px solid #000' }}> Continuar</p></Link>
             </div>
