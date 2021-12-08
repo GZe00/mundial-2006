787977449eab01bd05e72728eb33b7e844d4bd4b
Moises Fuentes <dev.gze.moises@gmail.com>
2021-12-08 14:22:37 -0600

    Complementacion de informacion generica

--- src/assets/paises/angola/informacion/informacion.js
+++ src/assets/paises/angola/informacion/informacion.js
@@ -1,9 +1,120 @@
+
 export const informacion = () => {
     return {
-        "nombreOficial": "Republica de Angola",
-        "pais": "Angola",
-        "poblacion": "32892",
-        "capital": "Berlin",
-        "moneda": "Euro"
+        "datosGenerales": {
+            "nombreOficial": "Federación Alemana",
+            "pais": "Alemania",
+            "poblacion": "32892",
+            "capital": "Berlin",
+            "moneda": "Euro",
+
+        }
+    }
+}
+export const informacionCompleta = () => {
+    return {
+        "datosGenerales": {
+            "pais": "Alemania",
+            "nombreOficial": "Federación Alemana",
+            "abreviatura": "",
+            "continente": "",
+            "capital": "Berlin",
+            "divisionPolitica":"",
+            "principalesCiudades": ["","","","","","","","","","",""],
+            "superficie":"",
+            "costas":"",
+            "puntoMasAlto": "",
+            "paisesVecinos": ["","","","","","","","",""],
+            "salidasAlMar":["",""],
+            "poblacion": "32892",
+            "pibPorHabitante":"",
+            "densidad":"",
+            "mediaDeEdad":"",
+            "esperanzaDeVida":"",
+            "alfabetismo":"",
+            "religion":["","",""],
+            "moneda": "Euro",
+            "idiomasOficiales":[""],
+            "fiestaNacional":"",
+            "codigoDeInternet": "",
+            "numeroDeInternautas":""
+        },
+        "grandesLeyendas": [{
+            "nombre": "Fritz Walter",
+            "fechaDeNacimiento": "31 de octubrede 1920",
+            "lugarDeNacimiento": "Kaiserlautern, Alemania",
+            "posicion": "Delantero",
+            "copasDelMundo": "2 (1954, 1958)",
+            "logrosMundiales": "Campeón (1954)",
+            "descripcioni": "Incididunt dolore qui quis culpa aliqua sit pariatur tempor aliquip labore elit. Commodo ullamco nostrud proident velit pariatur esse ea do esse reprehenderit. Aute in incididunt Lorem nostrud non Lorem occaecat elit incididunt anim id enim. Occaecat ea mollit nisi ipsum esse ex culpa sunt amet qui Lorem. Cillum adipisicing deserunt quis fugiat consectetur pariatur nulla."
+        }, {
+            "nombre": "Fritz Walter",
+            "fechaDeNacimiento": "31 de octubrede 1920",
+            "lugarDeNacimiento": "Kaiserlautern, Alemania",
+            "posicion": "Delantero",
+            "copasDelMundo": "2 (1954, 1958)",
+            "logrosMundiales": "Campeón (1954)",
+            "descripcioni": "Incididunt dolore qui quis culpa aliqua sit pariatur tempor aliquip labore elit. Commodo ullamco nostrud proident velit pariatur esse ea do esse reprehenderit. Aute in incididunt Lorem nostrud non Lorem occaecat elit incididunt anim id enim. Occaecat ea mollit nisi ipsum esse ex culpa sunt amet qui Lorem. Cillum adipisicing deserunt quis fugiat consectetur pariatur nulla."
+        }, {
+            "nombre": "Fritz Walter",
+            "fechaDeNacimiento": "31 de octubrede 1920",
+            "lugarDeNacimiento": "Kaiserlautern, Alemania",
+            "posicion": "Delantero",
+            "copasDelMundo": "2 (1954, 1958)",
+            "logrosMundiales": "Campeón (1954)",
+            "descripcioni": "Incididunt dolore qui quis culpa aliqua sit pariatur tempor aliquip labore elit. Commodo ullamco nostrud proident velit pariatur esse ea do esse reprehenderit. Aute in incididunt Lorem nostrud non Lorem occaecat elit incididunt anim id enim. Occaecat ea mollit nisi ipsum esse ex culpa sunt amet qui Lorem. Cillum adipisicing deserunt quis fugiat consectetur pariatur nulla."
+        }],
+        "federacion": {
+            "fundacion": "",
+            "afiliacion": "",
+            "participacionesEnLosMundiales": "",
+            "logrosMundiales": "",
+            "titulosContinentales": ""
+        },
+        "enLosMundiales": {
+            "descripcion": [
+                "Ea commodo mollit est veniam aliqua sunt eu minim esse. Qui ea ut enim sunt pariatur velit pariatur. Ex culpa pariatur proident irure fugiat. Adipisicing aute minim dolor deserunt labore ut aliquip mollit fugiat nulla fugiat sint.",
+                "Magna aute magna do elit id. Veniam pariatur officia deserunt do. Adipisicing culpa labore est ad nisi est adipisicing elit aliqua sunt Lorem sint. Dolor duis nisi officia ut ullamco esse aute laboris quis deserunt.",
+                "Ullamco aute incididunt exercitation occaecat est nisi tempor eiusmod Lorem veniam. Exercitation culpa quis voluptate proident culpa consequat duis proident id aliquip sint. Laborum et officia pariatur veniam commodo mollit deserunt. Excepteur eu eu sint proident. Minim ut consequat reprehenderit nisi ipsum laborum do occaecat ullamco consectetur mollit in Lorem. Lorem consequat nisi officia anim aliquip nostrud laboris minim sint magna consectetur commodo minim. Minim amet est esse magna sit magna ut eu."
+            ]
+        },
+        "formacion": {
+            "portero": ["Oliver Khan"],
+            "defensa": ["", "", "", ""],
+            "media": ["","","",""],
+            "delantera": ["",""],
+            "dt": ""
+        },
+        "datosCuriosos":[
+            "Aute tempor culpa eiusmod culpa sit veniam laborum ut. Sint ipsum ullamco do quis nulla. Ex aute aliquip et Lorem voluptate sunt ad ullamco ullamco aliquip amet ut. Deserunt culpa consectetur veniam nisi consequat magna id. In pariatur ex consectetur do minim qui qui do enim amet ex magna. Voluptate ea minim ipsum consequat elit sint proident. Dolore deserunt ullamco sunt sint ipsum cillum voluptate excepteur in.",
+            "Deserunt qui laboris excepteur pariatur. Consequat exercitation labore consectetur ad eiusmod qui officia nostrud voluptate. Id cillum est velit nisi est duis irure ut ipsum elit. Sunt anim nulla eiusmod nulla minim. Ipsum reprehenderit ut amet excepteur eu mollit Lorem sint duis et. Minim ex reprehenderit ex ad sunt veniam sint labore deserunt ea culpa esse duis cillum. Nisi anim esse dolore cillum ea tempor esse mollit eiusmod.",
+            "Id minim incididunt ad velit consectetur. Officia magna anim duis amet Lorem officia Lorem labore elit amet deserunt. Laboris nulla anim fugiat ea do reprehenderit consequat dolor commodo eu consectetur consequat. Anim proident nostrud consectetur pariatur. Id occaecat consequat esse laborum esse pariatur tempor."
+        ],
+        "figurasASeguir": [
+            {
+                "nombre": "",
+                "lugarDeNacimiento": "",
+                "posicion": "",
+                "copasMundiales":"",
+                "logrosMundiales":"",
+                "descripcion": ""
+            },
+            {
+                "nombre": "",
+                "lugarDeNacimiento": "",
+                "posicion": "",
+                "copasMundiales":"",
+                "logrosMundiales":"",
+                "descripcion": ""
+            },
+            {
+                "nombre": "",
+                "lugarDeNacimiento": "",
+                "posicion": "",
+                "copasMundiales":"",
+                "logrosMundiales":"",
+                "descripcion": ""
+            },
+        ]
     }
 }
